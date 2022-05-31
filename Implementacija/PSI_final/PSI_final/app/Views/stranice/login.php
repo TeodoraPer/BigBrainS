07cda92b35f9cea48808fe708ed0ec87c0bd6a76
anastasijav <volanastasija@gmail.com>
2022-05-31 15:06:05 +0200

    Anastasija required polja za login

--- Implementacija/PSI_final/PSI_final/app/Views/stranice/login.php
+++ Implementacija/PSI_final/PSI_final/app/Views/stranice/login.php
@@ -3,51 +3,56 @@
 // Anastasija Volcanovska 0092/19 izmena forme kako bi radila sa backendom
 ?>
 
-        <div class="row divCentar">
-            <div class="col-sm-1 " >
+<div class="row divCentar">
+    <div class="col-sm-1 " >
 
-            </div>
-            <div class="col-sm-10 centar" >
-              <div class="sredina">
-                  
-                
-                <div class="login">
-                    <form  method="post" action="<?= site_url("$controller/loginObrada") ?>">
-                      <table cellpadding="10px">
+    </div>
+    <div class="col-sm-10 centar" >
+        <div class="sredina">
+
+
+            <div class="login">
+                <form name="login_forma" method="post" action="<?= site_url("$controller/loginObrada") ?>">
+                    <table cellpadding="10px">
                         <tr>
-                          <td class="porukica1 error" >
-                            <?php 
-                                                        if(!empty($errors['greska'])) echo $errors['greska'];
-                                                        else echo "&nbsp;"?>
-                          </td>
-                        </tr>
-                       <tr>
-                          <td>   <input class="inpt" type="text" name="korime" value="" placeholder="Korisničko ime"></td>
+                            <td class="porukica1 error" >
+                                <?php
+                                if (!empty($errors['greska']))
+                                    echo $errors['greska'];
+                                else
+                                    echo "&nbsp;"
+                                    ?>
+                            </td>
                         </tr>
                         <tr>
-                          <td class="porukica1 error" >
-                          &nbsp;
-                          </td>
+                            <td>   <input class="inpt" type="text" name="korime" id="korime" required 
+                                          oninvalid="this.setCustomValidity('Morate uneti korisnicko ime!')" oninput="setCustomValidity('')" placeholder="Korisničko ime"/></td>
                         </tr>
-                       <tr>
-                           <td>   <input class="inpt" type="password" name="lozinka" value="" placeholder="Lozinka"></td>
+                        <tr>
+                            <td class="porukica1 error" >
+                                &nbsp;
+                            </td>
                         </tr>
-                      </table>
-                     
-                      <div class="Login_dugme">
+                        <tr>
+                            <td>   <input class="inpt" type="password" name="lozinka" id="lozinka" required 
+                                          oninvalid="this.setCustomValidity('Morate uneti lozinku!')" oninput="setCustomValidity('')" placeholder="Lozinka"/></td>
+                        </tr>
+                    </table>
+
+                    <div class="Login_dugme">
                         <button type="submit" class="button2">LOGIN</button><br>
-                        <a class="promena" href="#">Promena lozinke</a>
-                      </div>
-                    </form>
-                  </div>
-                  <br>
-      
-              
-                <hr class="linija">
-              </div>
+                        <a class="promena" href="<?= site_url("Gost/promenaLozinke") ?>">Promena lozinke</a>
+                    </div>
+                </form>
             </div>
-            <div class="col-sm-1 " >
-             
-            </div>
-         
+            <br>
+
+
+            <hr class="linija">
         </div>
+    </div>
+    <div class="col-sm-1 " >
+
+    </div>
+
+</div>
